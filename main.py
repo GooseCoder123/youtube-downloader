ee881167a0c4314fe741e8082039c8590ed42313
Vincent Kmin <vini.ms01@outlook.com>
2026-04-14 00:25:41 +0200

    added main code

---
+++ main.py
@@ -0,0 +1,14 @@
+import yt_dlp
+
+link = input("Enter the link: ")
+
+ydl_opts = {
+    'format': 'bestvideo',
+    'outtmpl': '%(title)s.%(ext)s',
+    'quiet': True
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    info = ydl.extract_info(link, download=False)
+    ydl.download([link])
+    print(f"'{info['title']}' downloaded!")
